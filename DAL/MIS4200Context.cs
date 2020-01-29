fe19a18a45e7d2e22e189530a05d0379d97797b4
dr874115 <dr874115@COB-5580LAB74>
2020-01-29 11:41:00 -0500

    1/29 class

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using DR874115_MIS4200.Models;
+using System.Linq;
+using System.Web;
+
+namespace DR874115_MIS4200.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+            // this method is a constructor and is called
+        }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<customer> Customers { get; set; }
+    }
+}
---
+++ Models/Orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DR874115_MIS4200.Models
+{
+    public class Orders
+    {
+        [Key]
+
+        public int orderNum { get; set; }
+        public string description { get; set; }
+        public DateTime orderDate { get; set; }
+        public int customerID { get; set; }
+        public virtual customer customer { get; set; }
+       
+    }
+}
