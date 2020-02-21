de15dfab53a4607bdc30baf15196c924e30ee90f
Devin Ruff <dr874115@ohio.edu>
2020-02-21 13:05:27 -0500

    SD 3

--- Models/Owners.cs
+++ Models/Owners.cs
@@ -11,10 +11,19 @@ namespace DR874115_MIS4200.Models
         [Key]
         public int OwnerID { get; set; }
         [Display(Name ="First Name")]
+        [Required(ErrorMessage ="Owner first name is required")]
+        [StringLength(20)]
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Owner last name is required")]
+        [StringLength(20)]
         public string LastName { get; set; }
+        [Display(Name = "Most used email address")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter your most frequently used email address")]
         public string Email { get; set; }
+        [Display(Name = "Mobile phone number")]
+        [DataType(DataType.PhoneNumber)]
         public string Phone { get; set; }
         public string City { get; set; }
         public ICollection<PetDetails> PetDetails { get; set; }
--- Models/Pets.cs
+++ Models/Pets.cs
@@ -10,8 +10,17 @@ namespace DR874115_MIS4200.Models
     {
         [Key]
         public int PetID { get; set; }
+        [Display(Name = "Pet Name")]
+        [Required(ErrorMessage = "Pet name is required")]
+        [StringLength(20)]
         public string PetName { get; set; }
+        [Display(Name = "Pet Type")]
+        [Required(ErrorMessage = "Pet type is required")]
+        [StringLength(40)]
         public string Type { get; set; }
+        [Display(Name = "Pet Breed")]
+        [Required(ErrorMessage = "Pet Breed is required")]
+        [StringLength(40)]
         public string Breed { get; set; }
         public ICollection<PetDetails> PetDetails { get; set; }
 
